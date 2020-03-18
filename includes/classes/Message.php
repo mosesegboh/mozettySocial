5ab48a0c0bcb6a5987920d494fe1eaba468890a4
mosesegboh <56415400+mosesegboh@users.noreply.github.com>
2020-03-18 03:03:02 +0200

    Created the upload pictures page
    Created the upload pictures page and made necessary changes as displayed
    
    Co-Authored-By: tkilanko <tkilanko@users.noreply.github.com>

--- includes/classes/Message.php
+++ includes/classes/Message.php
@@ -7,7 +7,26 @@
 			$this->con=$con; //this references the variables or properties of this particular class
 			$this->user_obj = new User($con, $user);//we are making an instance of the users class within
 		}//end of constructor
+	
+	public function getMostRecentUser(){
+		$userLoggedIn = $this->user_obj->getUsername(); 
+		$query = mysqli_query($this->con, 
+			"SELECT user_to, user_from FROM messages WHERE user_to='$userLoggedIn' OR user_from='$userLoggedIn' ORDER BY id DESC LIMIT 1")
+	
+		if (mysqli_num_rows($query)==0) 
+			return  false;
+
+			$row=mysqli_fetch_array($query);
+			$user_to = $row['user_to'];
+			$user_from = $row['user_from'];
+
+			if ($user_to != $userLoggedIn) 
+				return $user_to;
+			else 
+				return $user_from;
+			
 		
+	}
 	
 
 	}//end of the post class
